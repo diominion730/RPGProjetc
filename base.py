31def50f7a6072b18c0f71682d2685c3192b866b
diominion730 <viniciusgomesalves25@gmail.com>
2026-06-17 21:53:50 -0300

    Correções no while

--- base.py
+++ base.py
@@ -1,26 +1,26 @@
 #Chaves e valores iniciais obrigatorios
 heroi = {'Nivel': 0, 'Xp': 0}
 classe = {
-    "Classe1": {
-        'Nome': 'Guerreiro', 
-        "Força": 18,
-        "Defesa": 16,
-        "Vida": 150,
-        "Mana": 30
+  "Classe1": {
+      'Nome': 'Guerreiro', 
+      "Força": 18,
+      "Defesa": 16,
+      "Vida": 150,
+      "Mana": 30
     },
-    "Classe2":{
-        'Nome': 'Mago',
-        "Força": 6,
-        "Defesa": 8,
-        "Vida": 80,
-        "Mana": 180
+  "Classe2":{
+      'Nome': 'Mago',
+      "Força": 6,
+      "Defesa": 8,
+      "Vida": 80,
+      "Mana": 180
         },
     "Classe3":{
-        "Nome": "Arqueiro",
-        "Força": 12,
-        "Defesa": 10,
-        "Vida": 100,
-        "Mana": 60
+      "Nome": "Arqueiro",
+      "Força": 12,
+      "Defesa": 10,
+      "Vida": 100,
+      "Mana": 60
           }} 
 
 #Criação de personagem
@@ -31,33 +31,32 @@ heroi['Nome'] = nome_heroi
 condicaoClasse = True
 while condicaoClasse:
   print(
-"Selecione sua classe:\n"
-#Vinicius: Modifiquei colocando os nomes diretamente dos valores da lista
-f"1- {(classe['Classe1']['Nome'])} \n"
-f"2- {(classe['Classe2']['Nome'])} \n"
-f"3- {(classe['Classe3']['Nome'])} \n"
+  "Selecione sua classe:\n"
+  #Vinicius: Modifiquei colocando os nomes diretamente dos valores da lista
+  f"1- {(classe['Classe1']['Nome'])} \n"
+  f"2- {(classe['Classe2']['Nome'])} \n"
+  f"3- {(classe['Classe3']['Nome'])} \n"
  )
   
-  classeOp = int(input("Qual sera a classe do seu heroi:"))
+  classeOp = int(input("Qual sera a classe do seu heroi:")) #Permitir o usario digitar apenar 1 2 ou 3
   classeEscolhida = classe[f'Classe{classeOp}']
     
-  if classeOp == 1:
-    print(f"Voce selecionou a classe {classeEscolhida['Nome']}") #Mudança para evitar repetição
-    confirmacao = input("\nDeseja confirmar? S/N") #Permitir letras maiusculas e minusculas(precisa)
-    if confirmacao == "S" or confirmacao == "s" or confirmacao == "sim" or confirmacao == "Sim":
-      #Vinicius: Atribuindo status diretamento ao dic "heroi"
-      heroi['Classe'] = classeEscolhida['Nome']
-      heroi['Força'] = classeEscolhida['Força']
-      heroi['Defesa'] = classeEscolhida['Defesa']
-      heroi['Vida'] = classeEscolhida['Vida']
-      heroi['Mana'] = classeEscolhida['Mana']
+  print(f"Voce selecionou a classe {classeEscolhida['Nome']}") #Mudança para evitar repetição
+  confirmacao = input("\nDeseja confirmar? S/N") #Permitir letras maiusculas e minusculas(precisa)
+  if confirmacao == "S" or confirmacao == "s" or confirmacao == "sim" or confirmacao == "Sim":
+    #Vinicius: Atribuindo status diretamento ao dic "heroi"
+    heroi['Classe'] = classeEscolhida['Nome']
+    heroi['Força'] = classeEscolhida['Força']
+    heroi['Defesa'] = classeEscolhida['Defesa']
+    heroi['Vida'] = classeEscolhida['Vida']
+    heroi['Mana'] = classeEscolhida['Mana']
 
-      condicaoClasse = False
-    else:
-      continue
-    
+    condicaoClasse = False
+
+  else:
+    continue
+   
 print(heroi.items())
-
 ""
 #print("----MENU PRINCIPAL-----" \ IDEIA DE MENU SIMPLES
 #"1- Ver status" \
