017100b3a608fe1dd55c67817c67d8351fc37ae6
diominion730 <viniciusgomesalves25@gmail.com>
2026-06-17 21:42:25 -0300

    remove código duplicado na seleção de classes

--- base.py
+++ base.py
@@ -56,36 +56,6 @@ f"3- {(classe['Classe3']['Nome'])} \n"
     else:
       continue
     
-  if classeOp == 2:
-    #Vinicius: Arrumei a linha de baixo pq nao estava dando pra testar o resto do código
-    print(f"Voce selecionou a classe {classe['Classe2']['Nome']}") #LOGICA ERRADA
-    confirmacao = input("\nDeseja confirmar? S/N") #Permitir letras maiusculas e minusculas sem precisar tratar no "OR"
-    if confirmacao == "S" or confirmacao == "s" or confirmacao == "sim" or confirmacao == "Sim":
-      #Vinicius: Atribuindo status diretamento ao dic "heroi"
-      heroi['Classe'] = classe['Classe2']['Nome']
-      heroi['Força'] = classe['Classe2']['Força']
-      heroi['Defesa'] = classe['Classe2']['Defesa']
-      heroi['Vida'] = classe['Classe2']['Vida']
-      heroi['Mana'] = classe['Classe2']['Mana ']
-      condicaoClasse = False
-    else:
-      continue
-
-  if classeOp == 3:
-    #Vinicius: Arrumei a linha de baixo pq nao estava dando pra testar o resto do código
-    print(f"Voce selecionou a classe {classe['Classe3']['Nome']}")
-    confirmacao = input("\nDeseja confirmar? S/N") #Permitir letras maiusculas e minusculas 
-    if confirmacao == "S" or confirmacao == "s" or confirmacao == "sim" or confirmacao == "Sim":
-      #Vinicius: Inves de colocar a Dic "classe" dentro do diciniorio heroi, copio os atributos para a o Dic "heroi"
-      heroi['Classe'] = classe['Classe3']['Nome']
-      heroi['Força'] = classe['Classe3']['Força']
-      heroi['Defesa'] = classe['Classe3']['Defesa']
-      heroi['Vida'] = classe['Classe3']['Vida']
-      heroi['Mana'] = classe['Classe3']['Mana']
-      condicaoClasse = False
-    else:
-      continue
-
 print(heroi.items())
 
 ""
